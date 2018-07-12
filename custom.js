2c767e51af1ec4afe0afe3bdcb7cc5298931c3f5
emaileman2009 <41128073+emaileman2009@users.noreply.github.com>
2018-07-12 23:00:41 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -16,7 +16,7 @@ var addGetPremium_btn = function () {
   
   $(this.$getPremium_btn[0]).click(function( event ) {
               event.preventDefault();
-              essionStorage.setItem('premiumUser',true)
+              sessionStorage.setItem('premiumUser',true)
               location.reload();            
   });
   
